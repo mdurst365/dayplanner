61b79bd67144c63b573fffb0523c5963b9208f17
Molly Durst <mollymadegraphics@yahoo.com>
2020-11-11 15:48:46 -0500

    updated time to use moment.js

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,10 @@
 // JavaScript Document
 
 //Dynamic day of the week displayed at top of schedule
-/**
- * Function takes in a Date object and returns the day of the week in a text format.
- */
-function getWeekDay(date){
-    //Create an array containing each day, starting with Sunday.
-    var weekdays = new Array(
-        "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
-    );
-    //Use the getDay() method to get the day.
-    var day = date.getDay();
-    //Return the element that corresponds to that index.
-    return weekdays[day];
-}
-
-//The current weekday in a text format.
-var date = new Date();
-var weekDay = getWeekDay(date);
-document.getElementById("currentDay").innerHTML = weekDay;
-
-//Dynamic content for calendar here
+//updated to use moment.js
+$(document).ready(function() {
+// populates current date when dom is loaded
+var dateParagraph = $("#currentDay");
+var todayDate = moment().format('dddd, MMMM Do YYYY');
+dateParagraph.text(todayDate);
+});
