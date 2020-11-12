0223f879b87d651cfc38b9c13388007f795c5186
Molly Durst <mollymadegraphics@yahoo.com>
2020-11-11 20:49:50 -0500

    script is working now

--- assets/script.js
+++ assets/script.js
@@ -9,4 +9,30 @@ var todayDate = moment().format('dddd, MMMM Do YYYY');
 dateParagraph.text(todayDate);
 });
 
+//telling the user that their content is saved
+$(".saveBtn").click(function(){
+  alert("Content Saved");
+});
 
+//actually saving the content
+var input_eightAM = document.querySelector('#eightAM');
+var saveEightAM = document.querySelector('#saveEightAM');
+//retrieves previously save text to display
+input_eightAM.value = localStorage.getItem('content1');
+//when the Save button is clicked, store the input of the textarea
+saveEightAM.addEventListener('click', updateOutput1);
+//sets the input of text area into local storage
+function updateOutput1() {
+    localStorage.setItem('content1', input_eightAM.value);
+}
+
+var input_nineAM = document.querySelector('#nineAM');
+var saveNineAM = document.querySelector('#saveNineAM');
+//retrieves previously save text to display
+input_nineAM.value = localStorage.getItem('content1');
+//when the Save button is clicked, store the input of the textarea
+saveNineAM.addEventListener('click', updateOutput2);
+//sets the input of text area into local storage
+function updateOutput2() {
+    localStorage.setItem('content2', input_nineAM.value);
+}
